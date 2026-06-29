cd1083cad1a400fd75aa2db4b5bc588f1f345b30
vikalp1817243 <223328801+vikalp1817243@users.noreply.github.com>
2026-06-30 00:01:59 +0530

    add: /contact and /about endpoints

--- Day43-EJS/index.js
+++ Day43-EJS/index.js
@@ -7,6 +7,16 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
+app.get("/about", (req, res) => {
+    res.send("About Page");
+})
+app.get("/contact", (req, res) => {
+    res.send("Contact Me: john@example.com");
+})
+app.get("/about", (req, res) => {
+    res.send("About Me");
+})
+
 app.listen(port, () => { //port 3000
     console.log(`Server is running on port ${port}`);  //callback function
 });
