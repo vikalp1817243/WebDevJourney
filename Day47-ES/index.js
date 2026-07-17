dec1e6a6d27ae288490998e17ca5939984614bef
vikalp1817243 <223328801+vikalp1817243@users.noreply.github.com>
2026-07-17 21:27:31 +0530

    Update: getday()

--- Day47-ES/index.js
+++ Day47-ES/index.js
@@ -5,11 +5,18 @@ import { fileURLToPath } from "url";
 const app = express();
 const port = 3000;
 
+let today = new Date();
+let day = today.getDay();
+
 app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    dayType: "a weekday",
-    advice: "it's time to work hard",
-  });
+  let dayType = "the weekday";
+  let advice = "it's time to work hard";
+
+  if(day === 6 || day === 0) {
+    dayType = "the weekend";
+    advice = "it's time to relax";
+  }
+  res.render("index.ejs", { dayType, advice });
 });
 
 app.listen(port, () => {
